a58620a24b5c443dbbbccf54e166f4aa92d2a5f2
Matheus <MathieuBk0@gmail.com>
2024-04-11 11:49:27 +0100

    Updating CalculatorWithGUI.py

--- CalculatorWithGUI.py
+++ CalculatorWithGUI.py
@@ -6,30 +6,49 @@ class Calculator:
         self.expression = ""
         self.operation = StringVar()
 
-        self.GUI.configure(background="white")
+        self.GUI.configure(background="red")
         self.GUI.title("Calculator")
         self.GUI.geometry("375x375")
 
         self.create_widgets()
 
     def create_widgets(self):
-        self.expression_field = Entry(self.GUI, textvariable=self.operation, font=('Colibry', 15),
-                                      borderwidth=3, justify='right')
+        self.expression_field = Entry(
+            self.GUI, 
+            textvariable=self.operation, 
+            font=('Colibry', 15), 
+            borderwidth=3, 
+            justify='right'
+            )
         self.expression_field.grid(row=0, column=0, columnspan=4, pady=5, ipadx=50, ipady=10)
 
         buttons_data = [
-            ('7', '#ff0000'), ('8', '#ff7f00'), ('9', '#ffff00'), ('/', 'sky blue'),
-            ('4', '#7fff00'), ('5', '#00ff00'), ('6', '#00ff7f'), ('*', 'sky blue'),
-            ('1', '#00ffff'), ('2', '#007fff'), ('3', '#0000ff'), ('-', 'sky blue'),
-            ('C', 'sky blue'), ('0', '#7f00ff'), ('=', 'sky blue'), ('+', 'sky blue')
+            ('1', '#BB11FF'), ('2', '#BB11FF'), ('3', '#BB11FF'), ('+', '#4b9dff'),
+            ('4', '#BB11FF'), ('5', '#BB11FF'), ('6', '#BB11FF'), ('-', '#4b9dff'),
+            ('7', '#BB11FF'), ('8', '#BB11FF'), ('9', '#BB11FF'), ('*', '#4b9dff'),
+            ('C', 'sky blue'), ('0', '#FF964F'), ('=', '#08ff08'), ('/', '#4b9dff')
         ]
 
         row_num = 1
         col_num = 0
         for (text, bg_color) in buttons_data:
-            button = Button(self.GUI, text=text, fg='white', bg=bg_color, font=('Colibry', 15),
-                            height=2, width=7, borderwidth=0, relief="groove")
-            button.grid(row=row_num, column=col_num, pady=5, padx=5)
+            button = Button(
+                self.GUI, 
+                text=text, 
+                fg='white', 
+                bg=bg_color, 
+                font=('Colibry', 15), 
+                height=2, 
+                width=7, 
+                borderwidth=0, 
+                relief="groove"
+                )
+            button.grid(
+                row=row_num, 
+                column=col_num, 
+                pady=5, 
+                padx=5
+                )
 
             if text == 'C':
                 button.config(command=self.clear)
